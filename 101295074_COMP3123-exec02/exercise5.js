c2c57e6dafd2c3e6d7beb04f6d0f5d4904ecf786
Gadise oli <101295074@georgebrown.ca>
2025-09-22 19:42:08 -0500

    lab Exercise 03 commit and update

--- 101295074_COMP3123-exec02/exercise5.js
+++
@@ -1,8 +0,0 @@
-// exercise5.js
-const array = [1, 2, 3, 4];
-
-const calculateSum = array.reduce((sum, num) => sum + num, 0);
-const calculateProduct = array.reduce((product, num) => product * num, 1);
-
-console.log(calculateSum);     // Output: 10
-console.log(calculateProduct); // Output: 24
